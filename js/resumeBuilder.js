fc92df766f6153ac39f94300a12cc2a30e5f0e8e
MattHaugh <haughmatt@gmail.com>
2016-08-19 18:39:55 -0400

    Contact info changed from array to object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,11 +19,15 @@ var str3 = str2.replace(/u/i, "U");
 console.log(str3);
  */
  var skills = ["Java Experience", " C++ Experience", " JavaScript Experience", " Dish Washing" , " Food Prep"];
- var contactinfo = ["555-555-555", "@Matthaugh", "haughMatt@gmail.com" , "https://github.com/MattHaugh"];
  var bio = {
  	"name" : "Matt Haugh",
 	"role" : "Will code for Money",
-	"contact" : contactinfo,
+	"contact" : {
+		"mobil": "555-555-555",
+		"email": "haughMatt@gmail.com" ,
+		"twitter": "@Matthaugh",
+		"github": "https://github.com/MattHaugh"
+	},
 	"skills" : skills,
 	"photo" : "images/fry.jpg",
 	"welcome" : "Welcome to my Resume, I hope to fill it out a little more before it really gets used."
@@ -32,10 +36,10 @@ console.log(str3);
  var formattedRole = HTMLheaderRole.replace("%data%","Will Code for Money");
 
  var formattedPic = HTMLbioPic.replace("%data%", bio.photo);
- var formattedMobil = HTMLmobile.replace("%data%", contactinfo[0]);
- var formattedTwitter = HTMLtwitter.replace("%data%", contactinfo[1]);
- var formattedEmail = HTMLemail.replace("%data%", contactinfo[2]);
- var formattedGithub = HTMLgithub.replace("%data%", contactinfo[3]);
+ var formattedMobil = HTMLmobile.replace("%data%", contact.mobil;
+ var formattedTwitter = HTMLtwitter.replace("%data%", contact.twitter;
+ var formattedEmail = HTMLemail.replace("%data%", contact.email);
+ var formattedGithub = HTMLgithub.replace("%data%", contact.github);
  var formattedWelcome =  HTMLwelcomeMsg.replace("%data%", bio.welcome);
  var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
